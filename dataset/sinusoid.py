7ff0be4367e7d24ddc74dab6608dbf01eab40ae3
Yutao Chen <chnyutao@outlook.com>
2025-10-23 10:57:50 +0800

    fix sinusoid repeat

--- dataset/sinusoid.py
+++ dataset/sinusoid.py
@@ -21,9 +21,13 @@ def make_sinusoid_waves(n: int, *, key: PRNGKeyArray, **kwds: Any) -> jdl.DataLo
     """
     key1, key2, key2 = jr.split(key, 3)
     # generate data
-    x = jr.uniform(key1, (n,), minval=-2 * jnp.pi, maxval=2 * jnp.pi)
-    y1 = jnp.sin(x) + jr.normal(key2, (n,)) * 0.1
-    y2 = jnp.sin(x + jnp.pi) + jr.normal(key2, (n,)) * 0.1
+    x = jr.uniform(key1, (n, 1), minval=0, maxval=4 * jnp.pi)
+    y1 = jnp.sin(x) + jr.normal(key2, (n, 1)) * 0.01
+    y2 = jnp.sin(x + jnp.pi) + jr.normal(key2, (n, 1)) * 0.01
     # return
-    dataset = jdl.ArrayDataset(jnp.repeat(x, 2), jnp.concat([y1, y2]), asnumpy=False)
+    dataset = jdl.ArrayDataset(
+        jnp.concat([x, x]),
+        jnp.concat([y1, y2]),
+        asnumpy=False,
+    )
     return jdl.DataLoader(dataset, backend='jax', **kwds)
